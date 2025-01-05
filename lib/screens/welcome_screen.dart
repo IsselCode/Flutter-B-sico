5e4c1f0cb014c37aae2ff15c96d62a9cef950a00
issel <isselcode@gmail.com>
2025-01-05 17:33:24 -0600

    S5 C6 - Transiciones Personalizadas

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:example/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -6,14 +7,37 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.orange,
       appBar: AppBar(title: Text("Welcome Screen"),),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pushNamed(
+
+            // Navigator.pushNamed(
+            //   context,
+            //   "/login"
+            // );
+
+            Navigator.push(
               context,
-              "/login"
+              PageRouteBuilder(
+                transitionDuration: const Duration(seconds: 1),
+                pageBuilder: (context, animation, secondaryAnimation) => const LoginScreen(),
+                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(
+                    opacity: animation,
+                    child: SlideTransition(
+                      position: animation.drive(
+                        Tween(begin: const Offset(1.0, 0.0), end: Offset.zero)
+                            .chain(CurveTween(curve: Curves.easeInOutCirc))
+                      ),
+                      child: child,
+                    ),
+                  );
+                },
+              )
             );
+
           },
           child: Text("Ir al Login")
         ),
